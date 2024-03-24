9a12f5dc0484d7915e15983c5aa4d95d6615a22a
RahulKathayat <rkathayat1103@gmail.com>
2024-03-25 03:09:13 +0530

    new logo added

--- src/component/VTO/VTO.js
+++ src/component/VTO/VTO.js
@@ -5,17 +5,7 @@ const VTO = ({ setStatus, SetLogin }) => {
     <div>
       <div className="container">
         <div style={{ display: "flex", justifyContent: "center" }}>
-          <img src={'/assets/swaplogo.png'} height={60}/>
-          {/* <b
-            style={{
-              marginTop: "20px",
-              fontWeight: "bold",
-              fontSize: "30px",
-              fontFamily:"SoleilBold"
-            }}
-          >
-            VTO
-          </b> */}
+          <img src={'/assets/swaplogoblack.png'} height={60}/>
           &nbsp;&nbsp;
           <img
             src={"/assets/icons8-hanger-50.png"}
